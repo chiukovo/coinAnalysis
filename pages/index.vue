8cf1eec0312bcaf3cd46ba6c7f7f6bfafdfc9763
chiukovo <chiuko@celloes.com>
2021-04-22 17:27:10 +0800

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,8 @@
       <div class="bar">
         <div class="container px-4">
           <span>GMT+8</span>
-          <span class="bar__date">2021-04-20</span>
-          <span class="bar__time">15:14:21</span>
+          <span class="bar__date">{{ nowDay }}</span>
+          <span class="bar__time">{{ nowTime }}</span>
         </div>
       </div>
       <nav class="navbar">
@@ -70,6 +70,8 @@ import { mapState } from 'vuex'
 export default {
   data() {
     return  {
+      nowDay: '',
+      nowTime: '',
       containerId: 'main',
       pageList: [],
       tradingNotice: [],
@@ -77,6 +79,8 @@ export default {
   },
   mounted() {
     const _this = this
+
+    this.nowTimes()
 
     this.$list.onopen = function(e) {
       if (_this.$store.state.socket.reconnecting) {
@@ -266,7 +270,29 @@ export default {
       }
 
       return ''
-    }
+    },
+    timeFormate(timeStamp) {
+      let newdate = new Date(timeStamp)
+      let week = ['日', '一', '二', '三', '四', '五', '六']
+      
+      let year  = newdate.getFullYear()
+      let month = newdate.getMonth() + 1 < 10? "0" + (newdate.getMonth() + 1): newdate.getMonth() + 1
+      let date  = newdate.getDate() < 10? "0" + newdate.getDate(): newdate.getDate()
+      let hh    = newdate.getHours() < 10? "0" + newdate.getHours(): newdate.getHours()
+      let mm    = newdate.getMinutes() < 10? "0" + newdate.getMinutes(): newdate.getMinutes()
+      let ss    = newdate.getSeconds() < 10? "0" + newdate.getSeconds(): newdate.getSeconds()
+
+      this.nowTime = hh+":"+mm + ":" + ss
+      this.nowDay = year + "年" + month + "月" + date +"日"
+     },
+    // 定時器函數
+    nowTimes(){        
+      let self = this
+      self.timeFormate(new Date())
+      setInterval(function(){
+        self.timeFormate(new Date())
+      }, 1000)
+    },
   }
 }
 </script>
