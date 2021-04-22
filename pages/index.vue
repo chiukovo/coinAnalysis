e36dc5a3ff58f267cbb23ee09413824a76a2510d
chiukovo <chiuko@celloes.com>
2021-04-22 16:28:18 +0800

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -211,7 +211,7 @@ export default {
 
         _this.pageList = _this.pageList.map(list => {
           if (list.name + '/USDT' == newItem.name) {
-            list.action.push(newItem)
+            list.action.unshift(newItem)
           }
 
           countAction = list.action.length
