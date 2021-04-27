733bd59b16909b0ed5f1791524d19e9d001be7dd
chiukovo <chiuko@celloes.com>
2021-04-27 14:07:55 +0800

    add

--- pages/chat.vue
+++ pages/chat.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div id="tlkio" data-channel="coincoin543" data-theme="theme--minimal" data-custom-css="https://coincoin543.com/css/chat.css"></div>
+    <div id="tlkio" data-channel="coincoin543" data-theme="theme--minimal" data-custom-css="https://coincoin543.com/css/chat.css" style="height: 350px;"></div>
   </div>
 </template>
 
@@ -8,10 +8,11 @@
 export default {
   mounted() {
     const _this = this
+
     this.$nextTick(() => {
       this.$nuxt.$loading.finish()
     })
-  },
+  }
 }
 </script>
 
@@ -22,6 +23,5 @@ export default {
     bottom: 0;
     margin: 10px;
     border: 1px solid;
-    min-height: 300px;
   }
 </style>
